2e5cfc3cf424f99e90449606cd9f6e3efeed7dbb
ThorConzales <ThorConzales@users.noreply.github.com>
2017-09-08 09:57:00 +0300

    Version 1.3.0 release

--- Client/Client/src/app/shared/tracking.service.ts
+++ Client/Client/src/app/shared/tracking.service.ts
@@ -5,7 +5,7 @@ import { ConfigurationService } from './';
 @Injectable()
 export class TrackingService {
     private analytics;
-    private version: "1.2.1";
+    private version: "1.3.0";
 
     constructor(private configurationService: ConfigurationService) {
         try {
